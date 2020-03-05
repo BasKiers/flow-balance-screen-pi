5c0ffad97e931321193b4c997aff8723eb9be9ec
baskiers <bas.kiers@gmail.com>
2020-03-05 13:24:24 +0000

    delay setting screen id since mac address could not yet be available

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )
 
 echo "CLEANING"
-$SCRIPT_DIR/clean.sh
+#$SCRIPT_DIR/clean.sh
 
 echo "SET DEFAULT USER"
 
@@ -13,7 +13,6 @@ echo "SETUP"
 sudo adduser guest --gecos "guest guest, 10, 12, 12" --disabled-password
 #sudo useradd -m -d /home/guest/ -s /bin/bash -G guest
 echo "guest:foobar" | sudo chpasswd
-echo "pi:foobar" | sudo chpasswd
 sudo mkdir /home/guest
 sudo chown guest:guest /home/guest
 sudo chmod g+w /home/guest
@@ -21,7 +20,6 @@ sudo chmod o-rwx /home/*
 sudo usermod -aG guest pi
 
 sudo touch /home/guest/.screen_id
-sudo cat /sys/class/net/eth0/address | /usr/bin/md5sum | cut -f1 -d" " > /home/guest/.screen_id
 sudo chown guest:guest /home/guest/.screen_id
 sudo cp $SCRIPT_DIR/.xinitrc /home/guest/.xinitrc
 sudo cp $SCRIPT_DIR/.bash_profile /home/guest/.bash_profile
@@ -31,3 +29,5 @@ chown guest:guest /home/guest/.bash_profile
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends chromium-browser xserver-xorg x11-xserver-utils xinit openbox
 
+sleep 5
+sudo cat /sys/class/net/eth0/address | /usr/bin/md5sum | cut -f1 -d" " > /home/guest/.screen_id
